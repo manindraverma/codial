fc974e4c01e9e085f2b923ae41bca511f94de114
Manindra <manindra100@gmail.com>
2022-12-05 02:23:46 +0530

    check authentication on creating a post

--- routers/post.js
+++ routers/post.js
@@ -1,11 +1,12 @@
 const express =require('express');
 const router=express.Router();
+const passport=require('passport');
 
 //need to import the controller to get the action 
 const postController=require("../controllers/posts_controller");
 //now we can access all the action exported in the post controller
 //so to access we use below lines
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication, postController.create);
 
 module.exports=router;
 
